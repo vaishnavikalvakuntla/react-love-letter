713c06ac68ca39fe27000005fddfffe67dc2dedb
vaishnavikalvakuntla <40449740+vaishnavikalvakuntla@users.noreply.github.com>
2024-04-29 15:20:58 -0500

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -39,7 +39,7 @@ const LoveLetter = () => {
         I can't wait for the day we're together again. Even though we're apart, you're always in my thoughts and heart. Every day that passes, my love for you grows stronger. I miss the simple things we do together. You mean everything to me, and I can't wait to be with you again. Until then, know that I'm thinking of you always. 
 <br />This is a short letter to remind you that I love you. :)<br />
         always yours,<br />
-       Vaishnavi Kalvakuntla <3
+       Vaishnavi Kalvakuntla 
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
